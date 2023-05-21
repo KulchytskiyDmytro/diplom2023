36fa5e7d286afd112d90413d78a6c89f1f91f6e8
KulchytskiyDmytro <70943031+KulchytskiyDmytro@users.noreply.github.com>
2023-05-22 00:28:28 +0300

    page product

--- Shop/themes/e-store-theme/woocommerce/includes/wc-functions.php
+++ Shop/themes/e-store-theme/woocommerce/includes/wc-functions.php
@@ -5,3 +5,33 @@
  * Date: 03.11.2023
  * Time: 12:43
  */
+remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20 );
+add_action( 'woocommerce_before_main_content', 'estore_add_breadcrumbs', 20 );
+function estore_add_breadcrumbs(){
+	?>
+    <div class="sub-header">
+                <div class="sub-header__title icon-scuba subtitle">
+                    <!-- <a href="" class="subtitle _hover"> ВОДНЫЙ СПОРТ</a>
+                    <a href="" class="subtitle _hover"> ОЧКИ ДЛЯ ПЛАВАНИЯ</a>
+                    <span href="" class="subtitle"> EASYBREATH </span> -->
+                    <?php woocommerce_breadcrumb(); ?>
+                </div>
+            </div>
+<?php
+}
+
+add_action( 'woocommerce_before_single_product', 'estore_wrapper_product_start', 5 );
+function estore_wrapper_product_start() {
+	?>
+        <div class="product-page__content">
+            <div data-prodid="1" class="content">
+	<?php
+}
+
+add_action( 'woocommerce_after_single_product', 'estore_wrapper_product_end', 5 );
+function estore_wrapper_product_end() {
+	?>
+            </div>
+        </div>
+	<?php
+}
