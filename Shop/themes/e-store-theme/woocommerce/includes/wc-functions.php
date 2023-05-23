6829104c8369de6be093b6609893ca254506e28a
KulchytskiyDmytro <70943031+KulchytskiyDmytro@users.noreply.github.com>
2023-05-23 15:37:50 +0300

    wc-funk wrapper

--- Shop/themes/e-store-theme/woocommerce/includes/wc-functions.php
+++ Shop/themes/e-store-theme/woocommerce/includes/wc-functions.php
@@ -10,31 +10,30 @@ remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20 )
 add_action( 'woocommerce_before_main_content', 'estore_add_breadcrumbs', 20 );
 function estore_add_breadcrumbs(){
 	?>
-    <div class="sub-header container">
-                <div class="sub-header__title icon-scuba subtitle">
-                    <!-- <a href="" class="subtitle _hover"> ВОДНЫЙ СПОРТ</a>
-                    <a href="" class="subtitle _hover"> ОЧКИ ДЛЯ ПЛАВАНИЯ</a>
-                    <span href="" class="subtitle"> EASYBREATH </span> -->
-                    <?php woocommerce_breadcrumb(); ?>
-                </div>
+        <div class="sub-header container">
+            <div class="sub-header__title icon-scuba subtitle">
+                <?php woocommerce_breadcrumb(); ?>
             </div>
+        </div>
 <?php
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////
+remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10 );
 
-add_action( 'woocommerce_before_single_product', 'estore_wrapper_product_start', 5 );
-function estore_wrapper_product_start() {
+add_action( 'woocommerce_before_main_content', 'estore_output_content_wrapper_start', 10 );
+function estore_output_content_wrapper_start() {
 	?>
-        <div class="product-page__content container">
+        <div class="product-page__container container">
 	<?php
 }
 
-add_action( 'woocommerce_after_single_product', 'estore_wrapper_product_end', 5 );
-function estore_wrapper_product_end() {
+add_action( 'woocommerce_after_main_content', 'estore_output_content_wrapper_end', 10 );
+function estore_output_content_wrapper_end() {
 	?>
         </div>
 	<?php
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////
+add_action( 'woocommerce_before_single_product_summary', 'estore_output_content_wrapper_start', 10 );
